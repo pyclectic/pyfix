90b2366d0b6268ae2a505460969c2ecf6a8d6718
halimath <halimath.wilanthaou@googlemail.com>
2012-09-26 09:08:14 +0200

    re-imported some more classes

--- src/main/python/pyfix/__init__.py
+++ src/main/python/pyfix/__init__.py
@@ -5,3 +5,5 @@ __version__ = "${version}"
 from .cli import run_tests
 from .decorators import test, given
 from .fixture import Fixture, ConstantFixture, EnumeratingFixture, enumerate
+from .testcollector import TestCollector, TestDefinition
+from .testrunner import TestRunner, TestRunListener, TestResult, TestSuiteResult
