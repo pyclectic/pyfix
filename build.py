862ffa94fc9b07dea79cd223d45bdf7fa318e912
halimath <halimath.wilanthaou@googlemail.com>
2012-09-28 14:16:34 +0200

    added copyright notice

--- build.py
+++ build.py
@@ -1,3 +1,18 @@
+#  pyfix
+#  Copyright 2012 The pyfix team.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 """
 pyfix
 -----
@@ -42,7 +57,6 @@ license = "Apache Software License"
 
 @init
 def init (project):
-    project.build_depends_on("coverage")
     project.build_depends_on("mockito")
     project.depends_on("pyassert")
 
